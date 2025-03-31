bd9aa478b19f448cfdee4938e93ee8960e320134
anishatamne <142586823+anishatamne@users.noreply.github.com>
2025-03-31 20:53:29 +0530

    Update Admin.java

--- main/models/Admin.java
+++ main/models/Admin.java
@@ -1,3 +1,17 @@
-public class Admin {
-    
+// Admin.java
+
+package models;
+
+public class Admin extends User {
+    public Admin(String name, String email) {
+        super(name, email);
+    }
+
+    public void displayUserInfo() {
+        System.out.println("Admin: " + name + " (" + email + ")");
+    }
+
+    public void manageProducts() {
+        System.out.println(name + " is managing products.");
+    }
 }
