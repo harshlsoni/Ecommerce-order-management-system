f625fe9f0f903f0840c0b007bb6203ad49239dbd
anishatamne <142586823+anishatamne@users.noreply.github.com>
2025-03-31 20:54:25 +0530

    Update Customer.java

--- main/models/Customer.java
+++ main/models/Customer.java
@@ -1,3 +1,20 @@
-public class Customer {
-    
+//Customer.java 
+package models;
+import java.util.*;
+
+public class Customer extends User {
+    private List<Order> orders = new ArrayList<>();
+
+    public Customer(String name, String email) {
+        super(name, email);
+    }
+
+    public void placeOrder(Order order) {
+        orders.add(order);
+        System.out.println(name + " placed an order: " + order.getOrderId());
+    }
+
+    public void displayUserInfo() {
+        System.out.println("Customer: " + name + " (" + email + ")");
+    }
 }
