3c1b9f83e6e5de9fabdf9924a46b413e25179115
harshlsoni <harshlsoni2920@gmail.com>
2025-03-31 20:28:01 +0530

    Order.java modified

--- main/models/Order.java
+++ main/models/Order.java
@@ -1,3 +1,31 @@
+//Order.java
+
+
+package models;
+import java.util.*;
+
 public class Order {
-    
+    private String orderId;
+    private List<Product> products = new ArrayList<>();
+    private double totalAmount;
+
+    public Order(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalAmount += product.getPrice();
+    }
+
+    public String getOrderId() { return orderId; }
+    public double getTotalAmount() { return totalAmount; }
+
+    public void displayOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        for (Product p : products) {
+            System.out.println(" - " + p.getName() + " ($" + p.getPrice() + ")");
+        }
+        System.out.println("Total: $" + totalAmount);
+    }
 }
