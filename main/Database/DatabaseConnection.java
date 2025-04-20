bc49c6ab08a5151b765fea19430b9445df355a03
A-n-u-s-h-ka <165628531+A-n-u-s-h-ka@users.noreply.github.com>
2025-04-20 18:57:20 +0530

    Update DatabaseConnection.java

--- main/Database/DatabaseConnection.java
+++ main/Database/DatabaseConnection.java
@@ -1,14 +1,25 @@
-//DatabaseConnection.java
-
 package database;
-import java.sql.*;
+// --- DatabaseConnection.java ---
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "Manimau007@";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver loaded successfully.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+    }
 }
