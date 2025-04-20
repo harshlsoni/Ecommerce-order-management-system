48495cfc37088fa104d32c8687d913ab98ff36a5
anishatamne <142586823+anishatamne@users.noreply.github.com>
2025-04-20 19:23:36 +0530

    phase 2 Customer.java

--- main/models/Customer.java
+++ main/models/Customer.java
@@ -1,20 +1,22 @@
-//Customer.java 
 package models;
+import services.*;
 import java.util.*;
 
 public class Customer extends User {
     private List<Order> orders = new ArrayList<>();
 
     public Customer(String name, String email) {
-        super(name, email);
+        super(name, email, "Customer");
     }
 
     public void placeOrder(Order order) {
         orders.add(order);
+        order.saveToDatabase(email);
         System.out.println(name + " placed an order: " + order.getOrderId());
     }
 
     public void displayUserInfo() {
-        System.out.println("Customer: " + name + " (" + email + ")");
-    }
+        System.out.println("Customer: " + name + " (" + email + ")");
+    }
 }
+
